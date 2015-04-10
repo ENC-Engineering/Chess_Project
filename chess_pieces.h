c4e49e01ec7dde88d6b8a131adf80f7347744a94
Chris-moffitt <moffitt.a.chris@gmail.com>
2015-04-09 20:23:58 -0400

    Update chess_pieces.h

--- chess_pieces.h
+++ chess_pieces.h
@@ -471,9 +471,208 @@ class Queen : public Piece {
 public:
     Queen(bool isBlack) :Piece("queen", isBlack){}
     void move() {
-        
+        bool has_moved=false;
+        stack<int> x_cans;
+        stack<int> y_cans;
+        cout<<"Can move "
+        //up-right direction
+        int i=y_cor+1;
+        int j=x_cor+1
+        while(i<8 && j<8){
+            if (board[j][i]!=0){
+                if (board[j][i]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(j);
+                    y_cans.push(i);
+                    cout<<"("<<j<<","<<i<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(j);
+                y_cans.push(i);
+                cout<<"("<<j<<","<<i<<") ";
+            }
+            i++;
+            j++;
+        }
+        //down right direction
+        i=y_cor-1;
+        j=x_cor+1;
+        while(i>=0 && j<8){
+            if (board[j][i]!=0){
+                if (board[j][i]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(j);
+                    y_cans.push(i);
+                    cout<<"("<<j<<","<<i<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(j);
+                y_cans.push(i);
+                cout<<"("<<j<<","<<i<<") ";
+            }
+            i--;
+            j++;
+        }
+        //up left direction
+        i=x_cor-1;
+        j=y_cor+1;
+        while(i>=0 && j<8){
+            if (board[i][j]!=0){
+                if (board[i][j]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(i);
+                    y_cans.push(j);
+                    cout<<"("<<i<<","<<j<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(i);
+                y_cans.push(j);
+                cout<<"("<<i<<","<<j<<") ";
+            }
+            i--;
+            j++;
+        }
+        //down left direction
+        i=x_cor-1;
+        j=y_cor-1;
+        while(i>=0 && j>=0){
+            if (board[i][j]!=0){
+                if (board[i][j]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(i);
+                    y_cans.push(j);
+                    cout<<"("<<i<<","<<j<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(i);
+                y_cans.push(j);
+                cout<<"("<<i<<","<<j<<") ";
+            }
+            i--;
+            j--;
+        }
+        //up direction
+        i=y_cor+1;
+        while(i<8){
+            if (board[x_cor][i]!=0){
+                if (board[x_cor][i]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(x_cor);
+                    y_cans.push(i);
+                    cout<<"("<<x_cor<<","<<i<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(x_cor);
+                y_cans.push(i);
+                cout<<"("<<x_cor<<","<<i<<") ";
+            }
+            i++;
+        }
+        i=y_cor-1;
+        //down direction
+        while(i>=0){
+            if (board[x_cor][i]!=0){
+                if (board[x_cor][i]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(x_cor);
+                    y_cans.push(i);
+                    cout<<"("<<x_cor<<","<<i<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(x_cor);
+                y_cans.push(i);
+                cout<<"("<<x_cor<<","<<i<<") ";
+            }
+            i--;
+        }
+        //right direction
+        i=x_cor+1;
+        while(i<8){
+            if (board[i][y_cor]!=0){
+                if (board[i][y_cor]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(i);
+                    y_cans.push(y_cor);
+                    cout<<"("<<i<<","<<y_cor<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(i);
+                y_cans.push(y_cor);
+                cout<<"("<<i<<","<<y_cor<<") ";
+            }
+            i++;
+        }
+        //left direction
+        i=x_cor-1;
+        while(i>=0){
+            if (board[i][y_cor]!=0){
+                if (board[i][y_cor]->isBlack!=board[x_cor][y_cor]->isBlack){
+                    x_cans.push(i);
+                    y_cans.push(y_cor);
+                    cout<<"("<<i<<","<<y_cor<<") ";
+                }
+                break;
+            }
+            else {
+                x_cans.push(i);
+                y_cans.push(y_cor);
+                cout<<"("<<i<<","<<y_cor<<") ";
+            }
+            i--;
+        }
+        //Checks the stack of available moves and compares to where you want to move
+        int x_move_to=0;
+        int y_move_to=0;
+        bool x_input = false;
+        bool y_input = false;
+        //This while loop will be replaced with a "listen for click" function
+        while (!y_input || !x_input) {
+            x_move_to=0;
+            y_move_to=0;
+            cout<< "Where would you like to move (x,y)?"
+            int i=0;
+            line = getline();
+            for (int j=0; j<2; j++) {
+                while (!(line.at(i)>48 && line.at(i)<57)) {
+                    i++;
+                }
+                if (line.at(i) && j=0) {
+                    cin>>x_move_to;
+                    x_input = true;
+                }
+                else if (line.at(i) && j=1) {
+                    cin>> y_move_to;
+                    y_input = true;
+                }
+                else {
+                    cout<< "Invalid Input. Please enter in the form (x,y)";
+                    break;
+                }
+            
+            }
+        }
+        while(!x_cans.empty() && !y_cans.empty()){
+            if (x_move_to==x_cans.top() && y_move_to==y_cans.top()){
+                Piece* temp=board[x_cor][y_cor];
+                board[x_move_to][y_move_to]=board[x_cor][y_cor];
+                board[x_cor][y_cor]=0;
+                //Checks to make sure you didnt put yourself in check
+                has_moved=true;
+                //if checked yourself is true then move piece back and hasMoved = false
+                break;
+            }
+            x_cans.pop();
+            y_cans.pop();
+        }
+        return has_moved;
     }
-    
 };
 
 class King : public Piece {
